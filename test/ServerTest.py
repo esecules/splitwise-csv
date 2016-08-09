99566bde3ea7b36228eb80bfc048c1262a49b4c9
Eric Secules <e.secules@alumni.ubc.ca>
2016-08-09 14:07:45 -0700

    fixed test

--- test/ServerTest.py
+++ test/ServerTest.py
@@ -2,17 +2,13 @@ import unittest
 import requests
 import subprocess
 class ServerTest(unittest.TestCase):
-  def testRequest():
-    serverProc = subprocess.Popen(['python', '../src/server.py'], stdout=subprocess.PIPE, shell=True)
+  def test_request(self):
+    serverProc = subprocess.Popen(['python', '../src/server.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     test_data = "1234567890asdf"
-    resp = requests.get("http://localhost:5000?oauth_verifier=%s" % testData)
+    resp = requests.get("http://localhost:5000?oauth_verifier=%s" % test_data)
     assert resp.status_code == 200
-    try:
-      resp = requests.get("http://localhost:5000?oauth_verifier=%s" % testData)
-    except requests.exceptions.ConnectionError as e:
-      pass
-    else:
-      self.fail("Server responded to second request (It should only serve onece and die)")
+    with self.assertRaises(requests.ConnectionError):
+      resp = requests.get("http://localhost:5000?oauth_verifier=%s" % test_data)
     
     status = proc.poll()
     if status is None:  
