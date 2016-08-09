9bf4974df1c2f3975240074e8463913d222fabc2
Eric Secules <e.secules@alumni.ubc.ca>
2016-08-09 14:34:33 -0700

    fixing test, wait before server is up or down

--- test/ServerTest.py
+++ test/ServerTest.py
@@ -6,15 +6,19 @@ class ServerTest(unittest.TestCase):
   def test_request(self):
     serverProc = subprocess.Popen(['python', '../src/server.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     sleep(1)
-    self.assertEqual(None, serverProc.poll(), "Server exited unexpectedly")
+    self.assertEqual(None, serverProc.poll())
     test_data = "1234567890asdf"
     resp = requests.get("http://localhost:5000?oauth_verifier=%s" % test_data)
     assert resp.status_code == 200
     with self.assertRaises(requests.ConnectionError):
       resp = requests.get("http://localhost:5000?oauth_verifier=%s" % test_data)
     
-    status = serverProc.poll()
-    if status is None:  
+    polls = 0
+    while serverProc.poll() is None and polls < 10:
+      polls += 1
+      sleep(1)
+    
+    if serverProc.poll() is None:  
       serverProc.kill()
       self.fail("Server process is still alive, killing now...")
     stdout, stderr = serverProc.communicate()
